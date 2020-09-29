e8f87ab1518d527e322716f9f2e820b8da7aa63a
Roman <roman.zuchowski@gmail.com>
2020-09-29 18:10:15 +0200

    Updates

---
+++ precache-manifest.61c4f266172f09bff497cfd99bfbf4e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378ae4b1ba0c7c83c1fa610514e9072f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5ccdc54060ef90b860",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c730147a1317be5def8c",
+    "url": "/static/css/main.181f7d0a.chunk.css"
+  },
+  {
+    "revision": "2b5ccdc54060ef90b860",
+    "url": "/static/js/2.a955406e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a955406e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c730147a1317be5def8c",
+    "url": "/static/js/main.e16574c9.chunk.js"
+  },
+  {
+    "revision": "8c0b23a51e5a7fb6c301",
+    "url": "/static/js/runtime-main.645ae02d.js"
+  },
+  {
+    "revision": "89bad130842a7ac565cc7b1234008e13",
+    "url": "/static/media/construction.89bad130.jpg"
+  },
+  {
+    "revision": "ffdeb663cf8f70ad26f72caee16ee06b",
+    "url": "/static/media/roman.ffdeb663.png"
+  }
+]);
